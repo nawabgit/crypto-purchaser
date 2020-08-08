bc209d7be439afef17ee60a1ce451f00dda425fe
Nawab <nawab.mir14@gmail.com>
2020-08-09 00:44:37 +0100

    basic global theme applied to index.tsx

--- crypto-app/src/index.tsx
+++ crypto-app/src/index.tsx
@@ -2,9 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 
+import { createGlobalStyle } from "styled-components";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+
+  html, body {
+    margin: 0;
+    padding: 0;
+    background: #F7F8FB;
+    font-size: 14pt;
+    font-family: Roboto, sans-serif;
+  }
+
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: gray gray;
+  }
+  
+  *::-webkit-scrollbar {
+    width: 4px;
+  }
+  *::-webkit-scrollbar-thumb {
+    background-color: gray;
+    border-radius: 20px;
+  }
+`;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <GlobalStyle />
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById("root")
 );
