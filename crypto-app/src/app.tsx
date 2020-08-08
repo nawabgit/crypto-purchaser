9e54988c1a36072b47d257cf9ef1f1f1bcd8df7c
Nawab <nawab.mir14@gmail.com>
2020-08-09 00:44:46 +0100

    basic App component containers

--- crypto-app/src/app.tsx
+++ crypto-app/src/app.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const BackgroundContainer = styled.div`
+  display: flex;
+  min-height: 100vh;
+  max-height: 100vh;
+  background-color: white;
+`;
+
+const BitstampContainer = styled.div`
+  display: flex;
+  flex: 1;
+  border-right: 1px solid gray;
+`;
+
+const CoinbaseContainer = styled.div`
+  display: flex;
+  flex: 1;
+`;
 
 export function App() {
-  return <div>hello</div>;
+  return (
+    <BackgroundContainer>
+      <BitstampContainer></BitstampContainer>
+      <CoinbaseContainer></CoinbaseContainer>
+    </BackgroundContainer>
+  );
 }
 
 export default App;
