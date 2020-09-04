324d96877403eed899b2a6b2fa2a14539526a813
Nawab <nawab.mir14@gmail.com>
2020-09-04 01:42:24 +0100

    Updates

---
+++ precache-manifest.30f7ed6466d05f31d033217968713d46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11779c51fca0e5e1f9cfb92eea10ee90",
+    "url": "/crypto-purchaser/index.html"
+  },
+  {
+    "revision": "ad61b4c463977f7d436c",
+    "url": "/crypto-purchaser/static/js/2.fbda87a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/crypto-purchaser/static/js/2.fbda87a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0854d16fe5e561a7e029",
+    "url": "/crypto-purchaser/static/js/main.588c6a03.chunk.js"
+  },
+  {
+    "revision": "c52ed4ca49a3b3b471ca",
+    "url": "/crypto-purchaser/static/js/runtime-main.448372ee.js"
+  },
+  {
+    "revision": "692dcf7c2ab940ec3bc6b572867af6a4",
+    "url": "/crypto-purchaser/static/media/bitstamp-logo.692dcf7c.svg"
+  },
+  {
+    "revision": "3745fb4eff1e040eae4793b692f30c97",
+    "url": "/crypto-purchaser/static/media/coinbase-logo.3745fb4e.png"
+  },
+  {
+    "revision": "27e8ae6ababebde21bd64b758de9b577",
+    "url": "/crypto-purchaser/static/media/company-logo-cover.27e8ae6a.png"
+  }
+]);
